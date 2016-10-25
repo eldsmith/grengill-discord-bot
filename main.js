e1834dd1ae3e8312ed0f8d88315b5742edf89fa3
Fridberg <fridberg.traustason@gmail.com>
2016-10-25 05:34:07 +0000

    Forgot to change dir name

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const Grengill = require('./lib/grengill');
 const grengilBot = new Grengill(process.env.BOT_TOKEN);
 
 /*Require every app.js within subdirectories of modules and pass bot through*/
-var normalizedPath = require("path").join(__dirname, "modules");
+var normalizedPath = require("path").join(__dirname, "plugins");
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
   require('./plugins/' + file +'/app')(grengilBot);
 });
