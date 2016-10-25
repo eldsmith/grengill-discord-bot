1cd551f6bfd8a30ca6f456bc5530a2feb85b6ec4
Fridberg <fridberg.traustason@gmail.com>
2016-10-25 03:14:58 +0000

    Add dynamic load of plugins

--- main.js
+++ main.js
@@ -2,12 +2,18 @@
 require('dotenv').config();
 const Grengill = require('./lib/grengill');
 const grengilBot = new Grengill(process.env.BOT_TOKEN);
-require('./services/discordChat/discordChat')(grengilBot);
+
+/*Require every app.js within subdirectories of modules and pass bot through*/
+var normalizedPath = require("path").join(__dirname, "modules");
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  require('./plugins/' + file +'/app')(grengilBot);
+});
 
 grengilBot.on('ready', ()=>{
   console.log('GrengilBot ready');
 });
 
+/*FIXME:This should only catch ERRCONNECT, for now catches everything*/
 process.on('uncaughtException', (err)=>{
   console.error(err.stack);
   console.log("Node not stopping.");
