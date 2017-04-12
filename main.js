841452693d2b95451a334b7bf5c3e7d362767503
Fridberg <fridberg.traustason@gmail.com>
2017-04-12 01:18:37 +0000

    Some refactor code that works but really needs some changes

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 "use strict";
 require('dotenv').config();
 require('./lib/db/db').init(); //Initialize the database
+var express = require('express')
+var app = express();
 
-var app = require('express')();
+
+/*previous server.js*/
+app.set('view engine', 'pug'); //Sets the templating engine
+app.set('views', './plugins/grengill-web/public/views'); //Sets the views directory
+
+//Sets up a middleware that serves static pages from public
+app.use(express.static('./plugins/grengill-web/public'));
+
+require('./plugins/grengill-web/app/routes.js')(app); //passes in app to set up the routes
+
+/**/
+
+app.locals.env = process.env; // Make .env accessible to views
+
+// app.set('port', (process.env.PORT || 5000)); //Set the port to either the .env variable or 5000
+app.set('port', 3010); //Set the port to either the .env variable or 5000
+
+
+
 var http = require('http').Server(app);
 const Grengill = require('./lib/grengill');
 const grengilBot = new Grengill(process.env.BOT_TOKEN);
@@ -13,7 +33,7 @@ var normalizedPath = require('path').join(__dirname, 'plugins');
 
 try{
   require("fs").readdirSync(normalizedPath).forEach(function(file) {
-    require('./plugins/' + file +'/app')(grengilBot);
+    require('./plugins/' + file +'/app')(grengilBot, http);
   });
 }
 catch(error){
--- plugins/grengill-web/app.js
+++ plugins/grengill-web/app.js
@@ -1,17 +1,8 @@
 require('dotenv').config();
 
-var app = require('./app/server'); //import the contents of servers module.exports
+module.exports = (grengilBot, http) => {
 
-app.locals.env = process.env; // Make .env accessible to views
-
-app.set('port', (process.env.PORT || 5000)); //Set the port to either the .env variable or 5000
-
-app.listen(app.get('port'), function(){
-  console.log('server is running'); //Listen on the port and log success
-});
-
-
-// Just export an empty function as plugin importer expects a function return
-module.exports = () => {
-
+	http.listen(3010, function(){
+		console.log('server is running'); //Listen on the port and log success
+	});
 };
