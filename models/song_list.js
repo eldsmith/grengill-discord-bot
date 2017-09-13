74c3793eeef15718b09e6e3bf7684227b1218142
Friðberg Traustason <fridberg.traustason@gmail.com>
2017-09-13 17:02:17 +0000

    Add shuffled to songlists

--- models/song_list.js
+++ models/song_list.js
@@ -1,8 +1,15 @@
+const sortBy = require("lodash").sortBy;
+
 class SongList {
   constructor(songs = []) {
     this.songs = songs;
   }
 
+  /**
+   * returns a new shuffled list of songs
+   * @param  {Object} songs=this.songs
+   * @returns {SongList[]}
+   */
   mix(songs = this.songs) {
     let mixedList = [...songs];
     for (let i = mixedList.length; i >= 1; i--) {
@@ -11,6 +18,13 @@ class SongList {
     }
 
     return mixedList;
+  }
+  /**
+   * returns a static shuffled version of the list, does not reshuffle the list like mix
+   * @returns {SongList[]}
+   */
+  shuffled() {
+    return sortBy(this.songs, ["shuffleSeed"]);
   }
 }
 
