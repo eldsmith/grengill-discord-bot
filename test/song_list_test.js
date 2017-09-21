f45fa67009992307028c2e877ef0dcc295067b43
Friðberg Traustason <fridberg.traustason@gmail.com>
2017-09-21 21:53:12 +0000

    Unit testing for song lists init

--- test/song_list_test.js
+++ test/song_list_test.js
@@ -1,16 +1,97 @@
 const SongList = require("../models/song_list");
 const assert = require("assert");
+const err = require("../lib/errors");
 
-songs = [{ name: "1" }, { name: "2" }, { name: "3" }, { name: "4" }];
-
-const testList = new SongList({ songs });
+const songs = [{ name: "1" }, { name: "2" }, { name: "3" }, { name: "4" }];
 
 describe("SongList test", function() {
-  describe("Skipping", function() {
-    it("should go from first track to second", () => {
+  let testList;
+  describe("getNextTrack", function() {
+    before(function() {
+      testList = new SongList({ songs });
+    });
+
+    it("goes from 1 to 2", () => {
       let { song } = testList.getNextTrack({ skip: 1 });
-      console.log(song);
       assert(song.name === "2");
+    });
+    it("goes from 2 to 3", () => {
+      let { song } = testList.getNextTrack({ skip: 1 });
+      assert(song.name === "3");
+    });
+    it("goes from 3 to 1", () => {
+      let { song } = testList.getNextTrack({ skip: -2 });
+      assert(song.name === "1");
+    });
+    it("goes from 1 to 4", () => {
+      let { song } = testList.getNextTrack({ skip: 3 });
+      assert(song.name === "4");
+    });
+    it("tries to go less than 1 but still stay in 1", () => {
+      let { song } = testList.getNextTrack({ skip: -10 });
+      assert(song.name === "1");
+    });
+    it("loops to the beginning of the playlist and to 3", () => {
+      let { song } = testList.getNextTrack({ skip: 6 });
+      assert(song.name === "3");
+    });
+    it("loops and indicates that a loop occurred", () => {
+      let track = testList.getNextTrack({ skip: 6 });
+      assert(track.looped === true);
+    });
+    it("returns an error when nexting an empty playlist", () => {
+      let emptyList = new SongList({ songs: [] });
+
+      try {
+        emptyList.getNextTrack();
+      } catch (e) {
+        assert((e.error = err.PLAYLIST_EMPTY));
+      }
+    });
+  });
+
+  describe("Shuffle", function() {
+    before(function() {
+      testList = new SongList({ songs });
+    });
+
+    it("returns a properly shuffled playlist", function() {
+      let shuffled = testList.shuffle();
+      assert(shuffleTest(shuffled) === true);
+    });
+    it("returns an unshuffled playlist", function() {
+      let unshuffled = testList.get();
+      assert(shuffleTest(unshuffled) === false);
+    });
+  });
+
+  describe("Adding", function() {
+    before(function() {
+      testList = new SongList();
+    });
+
+    it("adds a song to an empty playlist", function() {
+      testList.add({ name: "bartholomew" });
+      assert(testList.get().length === 1);
+    });
+
+    it("adds another song to the end", function() {
+      testList.add({ name: "2" });
+      assert(testList.get()[1].name === "2");
     });
   });
 });
+
+function shuffleTest(songs) {
+  let shuffleWorked = true;
+  let prev = 0;
+
+  songs.map(song => {
+    if (song.shuffleSeed < prev) {
+      shuffleWorked = false;
+    }
+    prev = song.shuffleSeed;
+  });
+
+  return shuffleWorked;
+}
